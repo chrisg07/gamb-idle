cd704d6d09f77f41c831ed82f0f79455bcdcc7da
Christopher Guilliams <chrisg07@vt.edu>
2022-03-03 19:24:27 -0500

    lint fixes

--- apps/gamb-idle/src/app/shop/shop.component.ts
+++ apps/gamb-idle/src/app/shop/shop.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
-import { PhaserSingletonService } from '@spectacle-development/gamb-idle/phaser/singleton';
-import { SwordTypeEnum } from '@spectacle-development/shared/data-access-model';
 
 @Component({
     selector: 'openforge-shop',
@@ -11,35 +9,11 @@ import { SwordTypeEnum } from '@spectacle-development/shared/data-access-model';
 })
 export class ShopPageComponent implements OnInit {
     public isModal: boolean = false; // * Property to catch if component is on the modal or not
+
     constructor(private router: Router, private modalController: ModalController) {}
 
     ngOnInit(): void {
         console.log('ShopPageComponent ngOnInit');
-        this.checkIfModal();
-    }
-
-    /**
-     * * Purchase Sword
-     */
-    public async purchaseSword(type: string) {
-        const swordType = type === 'fancy' ? SwordTypeEnum.FANCY : SwordTypeEnum.CHEAP;
-        console.log('shop.component.ts', `Purchasing ${swordType} Sword...`);
-        await this.router.navigate(['/home']); // * Travel home first so that Phaser exists
-        PhaserSingletonService.shopObservable.next(swordType);
-        if (this.isModal) {
-            this.closeModal();
-        }
-    }
-
-    /**
-     * * Check if component was opened on a modal
-     */
-    public checkIfModal() {
-        void this.modalController.getTop().then(res => {
-            if (res) {
-                this.isModal = true;
-            }
-        });
     }
 
     /**
--- libs/shared/data-access-model/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/human/human.class';
-export * from './lib/blacksmith/blacksmith.class';
-export * from './lib/enums/sword.enum';
-export * from './lib/sword/sword.class';
